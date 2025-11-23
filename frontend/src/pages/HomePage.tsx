5b8c85252fcee060ae67f28e5218fa64702268d2
divyarao54 <divyasecondary@gmail.com>
2025-11-23 16:22:49 +0530

    Removed unused import from HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,5 +1,3 @@
-import React from 'react'
-import { Navigate } from 'react-router-dom'
 import Navbar from '../components/navbar';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
